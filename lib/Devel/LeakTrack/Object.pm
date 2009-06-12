3754d5c0ecff8f9fcb01f64b979507c2cc0e6888
Andy Armstrong <andy@hexten.net>
2009-06-12 17:41:55 +0100

    Reversion.

--- lib/Devel/LeakTrack/Object.pm
+++ lib/Devel/LeakTrack/Object.pm
@@ -9,7 +9,7 @@ use Scalar::Util ();
 use vars qw{ $VERSION @ISA @EXPORT_OK };
 use vars qw{ %OBJECT_COUNT %TRACKED %DESTROY_ORIGINAL %DESTROY_STUBBED %DESTROY_NEXT };
 BEGIN {
-	$VERSION     = '0.92';
+	$VERSION     = '0.1';
 
     # Set up exports
 	require Exporter;
